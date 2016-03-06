bd2605ef41da1a464b9fcc682e29a059b9cfb3e1
kyohei8 <tsukuda.kyouhei@gmail.com>
2016-03-06 18:58:12 +0900

    init

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var path = require('path')
+var port = process.env.PORT || 1337;
+
+var app = express()
+app.use(function (req, res, next) {
+    if (req.url.match(/.gz$/)) {
+        res.set('Content-Encoding', 'gzip')
+    }
+    if (req.url.match(/.js.gz$/)) {
+        res.set('Content-Type', 'text/javascript')
+    }
+
+    next()
+})
+app.use(express.static('.'))
+
+app.listen(port, function () {
+    console.log('listen on http://localhost:%s', port)
+})
